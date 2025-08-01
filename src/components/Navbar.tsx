e3bfd7855b31ff60ff5afee8834a24fcc3d2bb1f
Techxie-bc <donnaogbu@gmail.com>
2025-08-01 11:12:31 +0100

    font size for navbar adjusted

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,7 +32,7 @@ export default function Navbar() {
             <Image src="/images/IMSG-Logo.svg" alt="logo" width={50} height={20} className='object-contain' />
           </Link>
         </div>
-        <div className="font-satoshi space-x-6 gap-2 xl:gap-10 text-[16px] font-bold hidden lg:flex">
+        <div className="font-satoshi space-x-6 gap-2 xl:gap-10 text-[16px] md:text-[12px] xl:text-[16px] font-bold hidden lg:flex">
           {navLinks.map((link) => {
             const isActive =
               link.href === '/'
