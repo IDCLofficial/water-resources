3e3399913399f9e20525df51fde7c269f1698e8b
herdeybayor <shereefdeen4real@gmail.com>
2025-09-25 19:36:36 +0100

    Enable standalone output for images in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ["images.ctfassets.net"],
-    output: "standalone",
   },
+  output: "standalone",
 };
 
 export default nextConfig;
